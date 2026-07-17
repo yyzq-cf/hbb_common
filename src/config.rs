b081ea1b2a3a639dac7e91591132efdf25b1e3c8
有云转晴 <98072960+yyzq-cf@users.noreply.github.com>
2026-07-17 23:24:53 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ddns.ywsj365.com"];
+pub const RS_PUB_KEY: &str = "gHV1O3AZ0bNsB5wmQmxjIIeiICw9Q+gv2yIla1WuFXw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
